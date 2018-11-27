28e0e3517836704b1a60c53d5300008bdfd45451
Rogier Spieker <rogier.spieker@magneds.com>
2018-11-27 18:14:55 +0100

    Added inline documentation

--- source/index.js
+++ source/index.js
@@ -2,11 +2,33 @@ const Hapi = require('hapi');
 
 const storage = new WeakMap();
 
+/**
+ *  Convenience wrapper for the Hapi framework
+ *
+ *  @class HapiServer
+ */
 class HapiServer {
+	/**
+	 *  Creates an instance of HapiServer
+	 *
+	 *  @memberof HapiServer
+	 */
 	constructor() {
-		storage.set(this, { server: null, config: {}, plugins: [], routes: [] })
+		storage.set(this, {
+			server: null,
+			config: {},
+			plugins: [],
+			routes: [],
+		});
 	}
 
+	/**
+	 *  Add a set of configuration options, options that already exist will be overwritten
+	 *
+	 *  @param    {object}  args
+	 *  @returns  HapiServer
+	 *  @memberof HapiServer
+	 */
 	configure(...args) {
 		const { config } = storage.get(this);
 
@@ -16,37 +38,59 @@ class HapiServer {
 			}
 
 			if (typeof settings === 'object') {
-				Object.keys(settings)
-					.forEach((key) => config[key] = settings[key]);
+				Object.keys(settings).forEach(
+					(key) => (config[key] = settings[key])
+				);
 			}
-		})
+		});
 
 		//  allow for chaining
 		return this;
 	}
 
+	/**
+	 *  Register one or more plugins
+	 *
+	 *  @param     {object|[object]}  ...args
+	 *  @returns   HapiServer
+	 *  @memberof  HapiServer
+	 */
 	plugin(...args) {
 		const { plugins } = storage.get(this);
 
 		args.forEach((plugin) => {
 			plugins.push(...[].concat(plugin));
-		})
+		});
 
 		//  allow for chaining
 		return this;
 	}
 
+	/**
+	 *  Register one or more routes
+	 *
+	 *  @param     {object|[object]} ...args
+	 *  @returns   HapiServer
+	 *  @memberof  HapiServer
+	 */
 	route(...args) {
 		const { routes } = storage.get(this);
 
 		args.forEach((route) => {
 			routes.push(...[].concat(route));
-		})
+		});
 
 		//  allow for chaining
 		return this;
 	}
 
+	/**
+	 *  Start the Hapi server instance, registering all prepared plugins and routes
+	 *
+	 *  @async
+	 *  @returns   {object} server
+	 *  @memberof  HapiServer
+	 */
 	async start() {
 		const settings = storage.get(this);
 		const { config, plugins, routes } = settings;
